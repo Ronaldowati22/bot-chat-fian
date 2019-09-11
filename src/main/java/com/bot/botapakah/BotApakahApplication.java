73b2d51806ebe215b0fc2e8175a2348f7aee2180
Agung Wisnu Anggoro <agunganggoro98@gmail.com>
2019-09-11 20:37:37 +0700

    28

--- src/main/java/com/bot/botapakah/BotApakahApplication.java
+++ src/main/java/com/bot/botapakah/BotApakahApplication.java
@@ -91,7 +91,7 @@ public class BotApakahApplication extends SpringBootServletInitializer {
         Chat chatSession = new Chat(bot);
         
         String response = chatSession.multisentenceRespond(pesan);
-        String responsenospace = response.replace("\\s+\\s+\\s+\\s+\\s+\\s+\\s+\\s+\\s+", "");
+        String responsenospace = response.replaceAll("\\s+\\s+\\s+\\s+\\s+\\s+\\s+\\s+\\s+", "");
         System.out.println("Isi Response = "+responsenospace);
 
         balasChat(replyToken, responsenospace);
